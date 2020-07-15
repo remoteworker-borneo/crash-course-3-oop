46cabb1b2e2b8942855c191fa902f53a810d3c0d
coderot7 <mr.heeroe@gmail.com>
2020-07-15 22:10:30 +0800

    membuat class

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,11 @@
+class PersegiPanjang():
+    def __init__(self, p, l):
+        # fungsi yang dipanggil pertama kali saat object diciptakan
+        self.p = p
+        self.l = l
+    def info(self):
+        return f'Ini adalah object dari persegi panjang dengan panjang = {self.p} dan lebar = {self.l}'
+
+    def hitung_luas(self):
+        return self.p * self.l
+
